758824d1517a87a66191f0415fcaf692499a63e0
Christiannjhay <sugarolchristian8@gmail.com>
2024-11-09 13:15:18 +0800

    optimized current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,27 +4,18 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :current_user
   
-
   # Returns the current logged-in user (if any).
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    if (user_id = session[:user_id])  # Check if session has user_id
+      @current_user ||= User.find_by(id: user_id)  # Fetch user from database
+    end  # If no user_id in session, current_user remains nil
   end
-  
+
   # This method checks if the user is logged in. If not, it redirects them to the login page.
   def require_login
     if current_user.nil?
       redirect_to login_path, alert: 'Your session has expired. Please log in again.'
     end
   end
-
-
 
 end
